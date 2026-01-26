b9d226bfc50278fa1f5e8ed6230acec3fc3af008
shanti4shangrila <shanti4shangrila@yahoo.com>
2026-01-26 14:32:34 -0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // This ensures assets work correctly on GitHub Pages sub-paths
+  base: './', 
+})
